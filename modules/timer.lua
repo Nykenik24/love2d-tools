a5b00233e3a97da5cfab7504f39ea471cb112934
Nykenik24 <lucadoichita@gmail.com>
2024-12-19 18:10:13 +0100

    Made module "Timer" a class

--- modules/timer.lua
+++ modules/timer.lua
@@ -1,57 +1,63 @@
+local class = require("modules.class")
+
+local timer = {}
+timer.orig = 0
+timer.seconds = 0
+timer.rounded_seconds = math.floor(timer.seconds)
+
+timer.elapsed = false
+
+---Update the timer.
+---@param self table
+---@param dt integer
+---@return integer self.secs Time countdown.
+---@return boolean elapsed True if the timer ended.
+function timer:Update(dt)
+	if self.seconds > 0 then
+		self.seconds = self.secs - dt
+	elseif self.seconds < 0 then
+		self.seconds = 0
+		self.elapsed = true
+		return self.seconds, true
+	end
+	if self.elapsed then
+		self.elapsed = false
+		self:Reset()
+		self:OnEnd()
+	end
+
+	self.rounded_seconds = math.floor(self.seconds)
+	return self.seconds, false
+end
+
+---Reset the timer to the original duration (or another duration).
+---@param self table
+---@param new_duration? integer
+---@return integer self.secs Time countdown.
+function timer:Reset(new_duration)
+	if not new_duration then
+		self.seconds = self.orig
+	else
+		self.seconds = new_duration
+	end
+	self.elapsed = false
+	return self.seconds
+end
+
+timer.OnEnd = function(self)
+	print(("Timer %s ended!"):format(tostring(self)))
+end
+
+local timer_class = class(timer)
+
 ---Create a new timer.
 ---@param duration integer
----@return timer
+---@return table
 local function new(duration)
-	---@class timer
-	---@field orig integer The original duration.
-	---@field rounded_secs integer Time countdown (rounded).
-	---@field secs integer Time countdown.
-	---@field elapsed boolean Timer ended.
-	---@field update function
-	---@field reset function
-	---@field onEnd function Executed on timer end
-	return {
-		orig = duration,
-		rounded_secs = 0,
-		secs = duration,
-		elapsed = false,
-		---Update the timer.
-		---@param self timer
-		---@param dt integer
-		---@return integer self.secs Time countdown.
-		---@return boolean elapsed True if the timer ended.
-		update = function(self, dt)
-			if self.secs > 0 then
-				self.secs = self.secs - dt
-			elseif self.secs < 0 then
-				self.secs = 0
-				self.elapsed = true
-				return self.secs, true
-			end
-			if self.elapsed then
-				self.elapsed = false
-				self:reset()
-				self.onEnd()
-			end
-
-			self.rounded_secs = math.floor(self.secs)
-			return self.secs, false
-		end,
-		---Reset the timer to the original duration (or another duration).
-		---@param self timer
-		---@param new_duration? integer
-		---@return integer self.secs Time countdown.
-		reset = function(self, new_duration)
-			if not new_duration then
-				self.secs = self.orig
-			else
-				self.secs = new_duration
-			end
-			self.elapsed = false
-			return self.secs
-		end,
-		onEnd = function() end,
-	}
+	local timer_obj = timer_class:new()
+	timer_obj.seconds = duration
+	timer_obj.orig = duration
+	return timer_obj
 end
 
 return new
