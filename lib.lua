314e902c58cf0ffc2ff4da806c0c91878f1bc7f4
Nykenik24 <lucadoichita@gmail.com>
2024-12-22 11:57:00 +0100

    Cleaned the math module to create a "math" branch

--- lib.lua
+++ lib.lua
@@ -2,7 +2,6 @@ local Tools = {
 	debug = require("modules.debug"),
 	class = require("modules.class"),
 	timer = require("modules.timer"),
-	math = require("modules.math"),
 	lib_info = {
 		author = "Nykenik24",
 		url = "https://github.com/Nykenik24/love2d-tools",
